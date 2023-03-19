565c0a2a3f2c945960e67895574320a0eb421aa8
Fruuteze <113626939+Fruuteze@users.noreply.github.com>
2023-03-19 11:21:35 +0300

    Add files via upload

---
+++ next.py
@@ -0,0 +1,7 @@
+vip = input("Введите значение для min: ")
+arr_num = [vip] * 5
+print(arr_num)
+
+zont = input("Введите значение для max: ")
+arr_str = [zont] * 10
+print(arr_str)
---
+++ vip.py
@@ -0,0 +1,4 @@
+list1 = [[1,2,3,4], [1,2,3,5]]
+list2 = list1.copy()
+list2[0][3] = 100
+print(f'list1:{list1}\t list2{list2}')
